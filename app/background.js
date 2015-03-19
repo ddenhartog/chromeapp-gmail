c160a4065eaba421b6153f450dbb3ca079e541f6
Benjamin Denhartog <bddenhartog@gmail.com>
2015-03-19 12:06:44 -0700

    setup webview and background.js

--- app/background.js
+++ app/background.js
@@ -1,8 +1,26 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('app/frame.html', {
+  // Screen and window property variables
+  var screenw = screen.availWidth;
+  var screenh = screen.availHeight;
+  var width = 1400;
+  var height = 900;
+
+  var frame = chrome.app.window.create('app/frame.html', {
+    id: "frame",
     bounds: {
-      width: 1400,
-      height: 900
+      width: width,
+      height: height,
+      left: Math.round((screenw - width) / 2),
+      top: Math.round((screenh - height) / 2)
     }
+  },
+  function (win) {
+    win.contentWindow.onload = function() {
+      var webview = win.contentWindow.document.querySelector('#gmail-gui');
+      webview.addEventListener('newwindow', function(target) {
+        target.preventDefault();
+        window.open(target.targetUrl);
+      });
+    };
   });
 });
